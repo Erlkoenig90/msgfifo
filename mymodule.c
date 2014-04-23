44f7edec9549acfd355772f675681c0e3f742042
Niklas Guertler <profclonk@gmail.com>
2014-04-23 17:42:00 +0200

    Device-ID Registrierung

--- mymodule.c
+++ mymodule.c
@@ -1,18 +1,34 @@
 #include <linux/module.h>
 #include <linux/version.h>
+#include <linux/fs.h>
 
-static int mymodule_init (void) {
-	printk("Mymodule init\n");
+#include "msgfifo.h"
+
+static dev_t chrDevID;
+
+static int msgfifo_init (void) {
+	printk("MSG FIFO Module init\n");
+
+	if (alloc_chrdev_region (&chrDevID, 0, 1, "msgfifo") == -1) {
+		printk ("Device ID allocation failed!\n");
+		chrDevID = -1;
+	} else {
+		printk ("MSG FIFO Got device ID %d (%d:%d)\n", chrDevID, MAJOR (chrDevID), MINOR (chrDevID));
+	}
+
 	return 0;
 }
 
-static void mymodule_exit (void) {
-	printk("Mymodule exit\n");
+static void msgfifo_exit (void) {
+	printk("MSG FIFO Module exit\n");
+	if (chrDevID != -1) {
+		unregister_chrdev_region (chrDevID, 1);
+	}
 }
 
-module_init(mymodule_init);
-module_exit(mymodule_exit);
+module_init(msgfifo_init);
+module_exit(msgfifo_exit);
 
 MODULE_LICENSE("BSD"); 
 MODULE_AUTHOR("Niklas Guertler");
-MODULE_DESCRIPTION("Test module");
+MODULE_DESCRIPTION("Message FIFO test module");
